44111eb3811cbaf8243c6fb0170abb0e8c3dfdea
Kxy149 <k2750589048@126.com>
2026-05-27 22:20:51 +0800

    实验四

--- sy_4/TFLClassify/start/src/main/java/org/tensorflow/lite/examples/classification/MainActivity.kt
+++ sy_4/TFLClassify/start/src/main/java/org/tensorflow/lite/examples/classification/MainActivity.kt
@@ -27,24 +27,29 @@ import android.util.Log
 import android.util.Size
 import android.widget.Toast
 import androidx.activity.viewModels
+import androidx.annotation.OptIn
 import androidx.appcompat.app.AppCompatActivity
 import androidx.camera.core.Camera
 import androidx.camera.core.CameraSelector
+import androidx.camera.core.ExperimentalGetImage
 import androidx.camera.core.ImageAnalysis
 import androidx.camera.core.ImageProxy
 import androidx.camera.core.Preview
+import androidx.camera.core.resolutionselector.ResolutionSelector
+import androidx.camera.core.resolutionselector.ResolutionStrategy
 import androidx.camera.lifecycle.ProcessCameraProvider
 import androidx.camera.view.PreviewView
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
 import androidx.lifecycle.Observer
 import androidx.recyclerview.widget.RecyclerView
+import org.tensorflow.lite.examples.classification.ml.FlowerModel
 import org.tensorflow.lite.examples.classification.ui.RecognitionAdapter
 import org.tensorflow.lite.examples.classification.util.YuvToRgbConverter
 import org.tensorflow.lite.examples.classification.viewmodel.Recognition
 import org.tensorflow.lite.examples.classification.viewmodel.RecognitionListViewModel
+import org.tensorflow.lite.support.image.TensorImage
 import java.util.concurrent.Executors
-import kotlin.random.Random
 
 // Constants
 private const val MAX_RESULT_DISPLAY = 3 // Maximum number of results displayed
@@ -74,7 +79,7 @@ class MainActivity : AppCompatActivity() {
         findViewById<PreviewView>(R.id.viewFinder) // Display the preview image from Camera
     }
 
-    // Contains the recognition result. Since  it is a viewModel, it will survive screen rotations
+    // Contains the recognition result. Since it is a viewModel, it will survive screen rotations
     private val recogViewModel: RecognitionListViewModel by viewModels()
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -94,46 +99,33 @@ class MainActivity : AppCompatActivity() {
         val viewAdapter = RecognitionAdapter(this)
         resultRecyclerView.adapter = viewAdapter
 
-        // Disable recycler view animation to reduce flickering, otherwise items can move, fade in
-        // and out as the list change
+        // Disable recycler view animation to reduce flickering
         resultRecyclerView.itemAnimator = null
 
         // Attach an observer on the LiveData field of recognitionList
-        // This will notify the recycler view to update every time when a new list is set on the
-        // LiveData field of recognitionList.
         recogViewModel.recognitionList.observe(this,
             Observer {
                 viewAdapter.submitList(it)
             }
         )
-
     }
 
-    /**
-     * Check all permissions are granted - use for Camera permission in this example.
-     */
     private fun allPermissionsGranted(): Boolean = REQUIRED_PERMISSIONS.all {
         ContextCompat.checkSelfPermission(
             baseContext, it
         ) == PackageManager.PERMISSION_GRANTED
     }
 
-    /**
-     * This gets called after the Camera permission pop up is shown.
-     */
     override fun onRequestPermissionsResult(
         requestCode: Int,
         permissions: Array<String>,
         grantResults: IntArray
     ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
         if (requestCode == REQUEST_CODE_PERMISSIONS) {
             if (allPermissionsGranted()) {
                 startCamera()
             } else {
-                // Exit the app if permission is not granted
-                // Best practice is to explain and offer a chance to re-request but this is out of
-                // scope in this sample. More details:
-                // https://developer.android.com/training/permissions/usage-notes
                 Toast.makeText(
                     this,
                     getString(R.string.permission_deny_text),
@@ -144,57 +136,44 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
-    /**
-     * Start the Camera which involves:
-     *
-     * 1. Initialising the preview use case
-     * 2. Initialising the image analyser use case
-     * 3. Attach both to the lifecycle of this activity
-     * 4. Pipe the output of the preview object to the PreviewView on the screen
-     */
     private fun startCamera() {
         val cameraProviderFuture = ProcessCameraProvider.getInstance(this)
 
-        cameraProviderFuture.addListener(Runnable {
-            // Used to bind the lifecycle of cameras to the lifecycle owner
+        cameraProviderFuture.addListener({
             val cameraProvider: ProcessCameraProvider = cameraProviderFuture.get()
 
             preview = Preview.Builder()
                 .build()
 
+            // Setup resolution selector to replace deprecated setTargetResolution
+            val resolutionSelector = ResolutionSelector.Builder()
+                .setResolutionStrategy(
+                    ResolutionStrategy(
+                        Size(224, 224),
+                        ResolutionStrategy.FALLBACK_RULE_CLOSEST_HIGHER_THEN_LOWER
+                    )
+                )
+                .build()
+
             imageAnalyzer = ImageAnalysis.Builder()
-                // This sets the ideal size for the image to be analyse, CameraX will choose the
-                // the most suitable resolution which may not be exactly the same or hold the same
-                // aspect ratio
-                .setTargetResolution(Size(224, 224))
-                // How the Image Analyser should pipe in input, 1. every frame but drop no frame, or
-                // 2. go to the latest frame and may drop some frame. The default is 2.
-                // STRATEGY_KEEP_ONLY_LATEST. The following line is optional, kept here for clarity
+                .setResolutionSelector(resolutionSelector)
                 .setBackpressureStrategy(ImageAnalysis.STRATEGY_KEEP_ONLY_LATEST)
                 .build()
                 .also { analysisUseCase: ImageAnalysis ->
                     analysisUseCase.setAnalyzer(cameraExecutor, ImageAnalyzer(this) { items ->
-                        // updating the list of recognised objects
                         recogViewModel.updateData(items)
                     })
                 }
 
-            // Select camera, back is the default. If it is not available, choose front camera
             val cameraSelector =
                 if (cameraProvider.hasCamera(CameraSelector.DEFAULT_BACK_CAMERA))
                     CameraSelector.DEFAULT_BACK_CAMERA else CameraSelector.DEFAULT_FRONT_CAMERA
 
             try {
-                // Unbind use cases before rebinding
                 cameraProvider.unbindAll()
-
-                // Bind use cases to camera - try to bind everything at once and CameraX will find
-                // the best combination.
                 camera = cameraProvider.bindToLifecycle(
                     this, cameraSelector, preview, imageAnalyzer
                 )
-
-                // Attach the preview to preview view, aka View Finder
                 preview.setSurfaceProvider(viewFinder.surfaceProvider)
             } catch (exc: Exception) {
                 Log.e(TAG, "Use case binding failed", exc)
@@ -207,51 +186,44 @@ class MainActivity : AppCompatActivity() {
         ImageAnalysis.Analyzer {
 
         // TODO 1: Add class variable TensorFlow Lite Model
-        // Initializing the flowerModel by lazy so that it runs in the same thread when the process
-        // method is called.
-
-        // TODO 6. Optional GPU acceleration
-
+        private val flowerModel = FlowerModel.newInstance(ctx)
 
         override fun analyze(imageProxy: ImageProxy) {
 
             val items = mutableListOf<Recognition>()
 
             // TODO 2: Convert Image to Bitmap then to TensorImage
+            val tfImage = TensorImage.fromBitmap(toBitmap(imageProxy))
 
-            // TODO 3: Process the image using the trained model, sort and pick out the top results
+            // TODO 3: Process the image using the trained model
+            val outputs = flowerModel.process(tfImage)
+            val probability = outputs.probabilityAsCategoryList
 
             // TODO 4: Converting the top probability items into a list of recognitions
+            val topResults = probability.sortedByDescending { it.score }
+                .take(MAX_RESULT_DISPLAY)
+                .map { Recognition(it.label, it.score) }
 
-            // START - Placeholder code at the start of the codelab. Comment this block of code out.
-            for (i in 0 until MAX_RESULT_DISPLAY){
-                items.add(Recognition("Fake label $i", Random.nextFloat()))
-            }
-            // END - Placeholder code at the start of the codelab. Comment this block of code out.
+            items.addAll(topResults)
 
             // Return the result
             listener(items.toList())
 
-            // Close the image,this tells CameraX to feed the next image to the analyzer
+            // Close the image
             imageProxy.close()
         }
 
-        /**
-         * Convert Image Proxy to Bitmap
-         */
         private val yuvToRgbConverter = YuvToRgbConverter(ctx)
         private lateinit var bitmapBuffer: Bitmap
         private lateinit var rotationMatrix: Matrix
 
-        @SuppressLint("UnsafeExperimentalUsageError")
+        @OptIn(ExperimentalGetImage::class)
         private fun toBitmap(imageProxy: ImageProxy): Bitmap? {
 
             val image = imageProxy.image ?: return null
 
             // Initialise Buffer
             if (!::bitmapBuffer.isInitialized) {
-                // The image rotation and RGB image buffer are initialized only once
-                Log.d(TAG, "Initalise toBitmap()")
                 rotationMatrix = Matrix()
                 rotationMatrix.postRotate(imageProxy.imageInfo.rotationDegrees.toFloat())
                 bitmapBuffer = Bitmap.createBitmap(
@@ -273,7 +245,5 @@ class MainActivity : AppCompatActivity() {
                 false
             )
         }
-
     }
-
 }
